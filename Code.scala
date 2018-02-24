bab5cf831926493f7afd7c5354588237b752bbba
aramire4 <aramire4@xena16.cs.trinity.edu>
2018-02-24 15:47:37 -0600

    made it so that values can't be the same in the ArrayBuffer

--- Code.scala
+++ Code.scala
@@ -2,12 +2,14 @@ package via
 
 object Code {
   def make() = {
-    for(i <- 0 until 10){
+    for(i <- 0 until 1000){
       var s = scala.util.Random.alphanumeric.take(10).mkString
       s = s.toLowerCase
-      MyArrayBuffer.add((s, i+1))
+      if(!MyArrayBuffer.valueExists(s)){
+        MyArrayBuffer.add((s, i+1))
+      }
     }
-    MyArrayBuffer.add(("abcdefghij", 100))
+    MyArrayBuffer.add(("abcdefghij", 1001))
   }
   
 }
