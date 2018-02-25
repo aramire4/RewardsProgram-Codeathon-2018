600eec27d75d400afbe6d2574eba8e3282a14501
aramire4 <aramire4@xena16.cs.trinity.edu>
2018-02-24 18:15:57 -0600

    checked to see if the arrayBuffer could hold more in it (it can)

--- Code.scala
+++ Code.scala
@@ -2,14 +2,14 @@ package via
 
 object Code {
   def make() = {
-    for(i <- 0 until 1000){
+    for(i <- 0 until 10000){
       var s = scala.util.Random.alphanumeric.take(10).mkString
       s = s.toLowerCase
       if(!MyArrayBuffer.valueExists(s)){
-        MyArrayBuffer.add((s, i+1))
+        MyArrayBuffer.add((s, 10))
       }
     }
-    MyArrayBuffer.add(("abcdefghij", 1001))
+    MyArrayBuffer.add(("abcdefghij", 100))
   }
   
 }
